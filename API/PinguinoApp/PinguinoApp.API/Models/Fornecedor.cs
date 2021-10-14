581ca23be7932910f327ba9518a226b4dcae6267
Vandré Willian Priaro <vandrepriaro1@gmail.com>
2021-10-13 21:51:32 -0300

    Models
    Adicionei os models cliente, fornecedor e produto, criado no banco também

---
+++ API/PinguinoApp/PinguinoApp.API/Models/Fornecedor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PinguinoApp.API.Models
+{
+    public class Fornecedor
+    {
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Cnpjcpf { get; set; }
+
+        public string Cidade { get; set; }
+
+        public string Estado { get; set; }
+        public string Endereco { get; set; }
+        public string Email { get; set; }
+
+        public List<Produto> Produtos { get; set; }
+    }
+}
---
+++ API/PinguinoApp/PinguinoApp.API/Models/Produto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PinguinoApp.API.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Sku { get; set; }
+        public string Codigobarras { get; set; }
+        public Fornecedor Fornecedor { get; set; }
+
+    }
+}
