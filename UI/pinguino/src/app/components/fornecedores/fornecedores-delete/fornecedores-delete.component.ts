164f35a02988a7e1fcd8989a1c8954bb79c8a6f1
Suellen Lima <29680431+suellen002@users.noreply.github.com>
2021-10-28 22:08:21 -0300

    chore: implementação crud front fornecedor

--- UI/pinguino/src/app/components/fornecedores/fornecedores-delete/fornecedores-delete.component.ts
+++ UI/pinguino/src/app/components/fornecedores/fornecedores-delete/fornecedores-delete.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Fornecedor } from 'src/app/models/fornecedor';
+import { MessageBoxService } from 'src/app/services/message-box.service';
+import { FornecedoresService } from 'src/app/services/fornecedores.service';
 
 @Component({
   selector: 'app-fornecedores-delete',
@@ -7,9 +11,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FornecedoresDeleteComponent implements OnInit {
 
-  constructor() { }
+  fornecedor = new Fornecedor();
+
+  constructor(
+    private service: FornecedoresService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private message: MessageBoxService
+  ) { }
 
   ngOnInit(): void {
+    let id = this.route.snapshot.paramMap.get('id');
+    if ( id != null ) 
+      this.get(parseInt(id));
+  }
+
+  get(id: number): void {
+    let idAux: string = id.toString();
+    this.service.getOne(idAux).subscribe(
+      result => { this.fornecedor = result; }
+    )
+  }
+
+  save(): void {
+    this.service.delete(this.fornecedor.id).subscribe(
+      result => {        
+        this.message.show("Fornecedor Excluído com Sucesso.");
+        this.router.navigate(["/fornecedores"]);
+      }
+    )
+  }
+
+  cancel(): void {
+    this.router.navigate(['/fornecedores']);
   }
 
 }
+
